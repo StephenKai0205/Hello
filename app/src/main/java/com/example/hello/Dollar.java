a2d2fbf8aaa9f9fb9380060f4efce1fc6c0044fc
StephenKai0205 <568842511@qq.com>
2020-09-28 21:58:17 +0800

    添加dollar setrate

--- app/src/main/java/com/example/hello/Dollar.java
+++ app/src/main/java/com/example/hello/Dollar.java
@@ -1,5 +1,7 @@
 package com.example.hello;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,41 +13,48 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Dollar extends AppCompatActivity {
     TextView out;
     EditText edit;
+    double dollar;
+    double euro;
+    double won;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dollar);
-        EditText input = findViewById(R.id.EditTextTextPersonName2);
-        String str = input.getText().toString();
-        out = (TextView) findViewById(R.id.textView4);
-        edit = (EditText) findViewById(R.id.EditTextTextPersonName2);
+        dollar = 0.1465;
+        euro = 0.1259;
+        won = 171.7179;
+
+
     }
     public void exchange(View v){
+        out = (TextView) findViewById(R.id.textView5);
+        edit = (EditText) findViewById(R.id.EditTextTextPersonName2);
+        Intent intent = getIntent();
         if(v.getId()==R.id.button3){
             Double d = Double.parseDouble(edit.getText().toString());
-            out.setText(""+d*1);
+            out.setText("结果为："+d*dollar);
         }else if(v.getId()==R.id.button5){
             Double d = Double.parseDouble(edit.getText().toString());
-            out.setText(""+d*2);
+            out.setText("结果为："+d*euro);
 
         }else if(v.getId()==R.id.button7){
             Double d = Double.parseDouble(edit.getText().toString());
-            out.setText(""+d*3);
+            out.setText("结果为："+d*won);
 
         }
     }
     public void dollarrate(View v){
-        String str = edit.getText().toString();
-        Double d = Double.parseDouble(str);
-        d=d*1.8+32;
-        out.setText("结果为："+ d);
+       String str = edit.getText().toString();
+       Double d = Double.parseDouble(str);
+       out.setText("结果为："+ d);
 
     }
-    public void eurorate(View v){
+ public void eurorate(View v){
         String str = edit.getText().toString();
-        Double d = Double.parseDouble(str);
-        d=d*1.8+32;
-        out.setText("结果为："+ d);
+      Double d = Double.parseDouble(str);
+       d=d*1.8+32;
+       out.setText("结果为："+ d);
 
     }
     public void wonrate(View v){
@@ -56,10 +65,29 @@ public class Dollar extends AppCompatActivity {
 
     }
     public void jumpsetrate(View v){
-        Intent intent = new Intent();
-        intent.setClass(Dollar.this, Setrate.class);
-        startActivity(intent);
+        Intent intent = new Intent(this,Setrate.class);
+        Bundle bdl = new Bundle();
+       // startActivity(intent);
+        //startActivityForResult(intent,0);
+
+
+        bdl.putDouble("key_dollar",dollar);
+        bdl.putDouble("key_euro",euro);
+        bdl.putDouble("key_won",won);
+        intent.putExtras(bdl);
+        startActivityForResult(intent,0);
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==0&&resultCode==0){
+            Bundle bd = data.getExtras();
+            dollar=bd.getDouble("key_dollar",0);
+            euro=bd.getDouble("key_euro",0);
+            won=bd.getDouble("key_won",0);
+
+        }
+    }
 }
--- app/src/main/java/com/example/hello/Setrate.java
+++ app/src/main/java/com/example/hello/Setrate.java
@@ -1,5 +1,6 @@
 package com.example.hello;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -13,27 +14,42 @@ public class Setrate extends AppCompatActivity {
     EditText edit1;
     EditText edit2;
     EditText edit3;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setrate);
-        EditText input1 = findViewById(R.id.editTextTextPersonName6);
-        String str1 = input1.getText().toString();
-        EditText input2 = findViewById(R.id.editTextTextPersonName5);
 
-        String str2 = input1.getText().toString();
-        EditText input3 = findViewById(R.id.editTextTextPersonName);
-        String str3 = input1.getText().toString();
-        //out = (TextView) findViewById(R.id.textView5);
+
         edit1 = (EditText) findViewById(R.id.editTextTextPersonName6);
         edit2 = (EditText) findViewById(R.id.editTextTextPersonName5);
         edit3 = (EditText) findViewById(R.id.editTextTextPersonName);
+        Intent intent = getIntent();
+        Bundle bundle =intent.getExtras();
+        double dollar =bundle.getDouble("key_dollar",0.1);
+        double euro =bundle.getDouble("key_euro",0.1);
+        double won =bundle.getDouble("key_won",0.1);
+        edit1.setText(dollar+"");
+        edit2.setText(euro+"");
+        edit3.setText(won+"");
 }
-    public void setrate(View v){
+
+    public void getrate(View v){
+        edit1 = (EditText) findViewById(R.id.editTextTextPersonName6);
+        edit2 = (EditText) findViewById(R.id.editTextTextPersonName5);
+        edit3 = (EditText) findViewById(R.id.editTextTextPersonName);
+        Intent intent = new Intent(this,Dollar.class);
+        Bundle bdl = new Bundle();
         String str1 = edit1.getText().toString();
-        Double d = Double.parseDouble(str1);
-        d=d*1.8+32;
-        out.setText("结果为："+ d);
+        String str2 = edit2.getText().toString();
+        String str3 = edit3.getText().toString();
+        bdl.putDouble("key_dollar",Double.parseDouble(str1));
+        bdl.putDouble("key_euro",Double.parseDouble(str2));
+        bdl.putDouble("key_won",Double.parseDouble(str3));
+        intent.putExtras(bdl);
+        setResult(0,intent);
+        finish();
+
 
     }
 
